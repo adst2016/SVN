d7fcdc5aec224500ad33a51e591a9955e55c0ca6
adst2016 <adam.stachura@hotmail.com>
2016-10-17 18:50:30 +0200

    Implementation of Chain of responsibility design pattern.

---
+++ DesignPatterns/DesignPatterns/ChainOfResponsibility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class Handler
+    {
+        protected Handler Succesor;
+
+        public void SetSuccesor(Handler succesor)
+        {
+            this.Succesor = succesor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if(request >= 0 && request < 10)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                this.GetType().Name, request);
+            }
+            else if(Succesor != null)
+            {
+                this.Succesor.HandleRequest(request);
+            }            
+        }
+    }
+
+    public class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                this.GetType().Name, request);
+            }
+            else if (Succesor != null)
+            {
+                this.Succesor.HandleRequest(request);
+            }
+        }
+    }
+
+    public class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 20 && request < 30)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                this.GetType().Name, request);
+            }
+            else if (Succesor != null)
+            {
+                this.Succesor.HandleRequest(request);
+            }
+        }
+    }
+
+}
---
+++ DesignPatterns/DesignPatternsTests/ChainOfResponsibilityTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns;
+
+namespace DesignPatternsTests
+{
+    [TestClass]
+    public class ChainOfResponsibilityTests
+    {
+        [TestMethod]
+        public void ChainResponsibilityTest_01()
+        {
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            Handler h3 = new ConcreteHandler3();
+
+            h1.SetSuccesor(h2);
+            h2.SetSuccesor(h3);
+
+            int[] requests = { 2, 5, 14, 22, 38, 3, 27, 20 };
+
+            foreach (int request in requests)
+            {
+                h1.HandleRequest(request);
+            }
+        }
+    }
+}
