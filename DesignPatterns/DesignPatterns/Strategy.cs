fe6bda556a453b3dfde98e67a7ad637a8370c9e9
adst2016 <adam.stachura@hotmail.com>
2016-11-24 21:10:01 +0100

    Dodanie wzorca strategii.

---
+++ DesignPatterns/DesignPatterns/Strategy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    public class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("ConcreteStrategyA");
+        }
+    }
+
+    public class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("ConcreteStrategyB");
+        }
+    }
+
+    public class Context
+    {
+        private Strategy _strategy;
+        public Context(Strategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            this._strategy.AlgorithmInterface();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatternsTests/StrategyTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns;
+
+namespace DesignPatternsTests
+{
+    [TestClass]
+    public class StrategyTests
+    {
+        [TestMethod]
+        public void StrategyTest_01()
+        {
+            Context context;
+
+            Strategy strategyA = new ConcreteStrategyA();
+            context = new Context(strategyA);
+            context.ContextInterface();
+
+            Strategy strategyB = new ConcreteStrategyB();
+            context = new Context(strategyB);
+            context.ContextInterface();
+
+        }
+    }
+}
