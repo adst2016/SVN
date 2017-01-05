b426d8282f6bc7d760834c91a733a0560987874a
adst2016 <adam.stachura@hotmail.com>
2017-01-05 13:30:04 +0100

    Observer design pattern.

---
+++ DesignPatterns/DesignPatterns/BehavioralPatterns/Observer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.BehavioralPatterns
+{
+    public abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in _observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    public class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        public string  SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+
+    }
+
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    public class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            _subject = subject;
+            _name = name;
+        }
+
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+
+            Console.WriteLine("Observer {0}'s new state is {1}", _name, _observerState);
+        }
+
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+
+    }
+}
---
+++ DesignPatterns/DesignPatternsTests/BehavioralPatterns/ObserverTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.BehavioralPatterns;
+
+namespace DesignPatternsTests.BehavioralPatterns
+{
+    [TestClass]
+    public class ObserverTests
+    {
+        [TestMethod]
+        public void ObserverTest()
+        {
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            s.SubjectState = "ABC";
+            s.Notify();
+
+        }
+    }
+}
