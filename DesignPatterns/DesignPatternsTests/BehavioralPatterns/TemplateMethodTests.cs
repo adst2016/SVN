e5b298cd1c06f9d9b5a0bc3b8111a7ad4c77efe2
adst2016 <adam.stachura@hotmail.com>
2017-01-05 15:26:02 +0100

    TemplateMethod design pattern.

---
+++ DesignPatterns/DesignPatternsTests/BehavioralPatterns/TemplateMethodTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.BehavioralPatterns;
+
+namespace DesignPatternsTests.BehavioralPatterns
+{
+    [TestClass]
+    public class TemplateMethodTests
+    {
+        [TestMethod]
+        public void TemplateMethodTest()
+        {
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
+
+        }
+    }
+}
