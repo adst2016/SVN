7434b6f6d3fa0145858d48fa8248b4a9d67fe8cb
adst2016 <adam.stachura@hotmail.com>
2017-01-05 13:44:38 +0100

    State design pattern.

---
+++ DesignPatterns/DesignPatterns/BehavioralPatterns/State.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DesignPatterns.BehavioralPatterns
+{
+    public abstract class State
+    {
+        public abstract void Handle(StateContext context);
+    }
+
+    public class ConcreteStateA : State
+    {
+        public override void Handle(StateContext context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    public class ConcreteStateB : State
+    {
+        public override void Handle(StateContext context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    public class StateContext
+    {
+        private State _state;
+
+        public StateContext(State state)
+        {
+            _state = state;
+        }        
+
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State: " + _state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatternsTests/BehavioralPatterns/StateTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.BehavioralPatterns;
+
+namespace DesignPatternsTests.BehavioralPatterns
+{
+    [TestClass]
+    public class StateTests
+    {
+        [TestMethod]
+        public void StateTest()
+        {
+            StateContext c = new StateContext(new ConcreteStateA());
+
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
+
+        }
+    }
+}
