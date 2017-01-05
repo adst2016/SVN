72ddbb7776d349b0eca780277c335a2f675460b6
adst2016 <adam.stachura@hotmail.com>
2017-01-05 15:40:01 +0100

    Visitor design pattern.

---
+++ DesignPatterns/DesignPatternsTests/BehavioralPatterns/VisitorTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.BehavioralPatterns;
+
+namespace DesignPatternsTests.BehavioralPatterns
+{
+    [TestClass]
+    public class VisitorTests
+    {
+        [TestMethod]
+        public void VisitorTest()
+        {
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
+
+            ConcreteVisitor1 v1 = new ConcreteVisitor1();
+            ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+            o.Accept(v1);
+            o.Accept(v2);
+
+        }
+    }
+}
