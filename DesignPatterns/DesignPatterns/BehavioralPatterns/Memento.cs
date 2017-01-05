071c23240e4d4c4a849bd5dca1557b124dc4a2e7
adst2016 <adam.stachura@hotmail.com>
2017-01-05 13:08:57 +0100

    Memento design pattern.

---
+++ DesignPatterns/DesignPatterns/BehavioralPatterns/Memento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesignPatterns.BehavioralPatterns
+{
+    public class Originator
+    {
+        private string _state;
+
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State = " + _state);
+            }
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(_state);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+
+    }
+
+    public class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            _state = state;
+        }
+
+        public string State
+        {
+            get { return _state; }
+        }
+    }
+
+    public class Caretaker
+    {
+        private Memento _memento;        
+
+        public Memento Memento
+        {
+            get { return _memento; }
+            set { _memento = value; }
+        }
+
+    }
+}
---
+++ DesignPatterns/DesignPatternsTests/BehavioralPatterns/MementoTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.BehavioralPatterns;
+
+namespace DesignPatternsTests.BehavioralPatterns
+{
+    [TestClass]
+    public class MementoTests
+    {
+        [TestMethod]
+        public void MementoTest()
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            o.State = "Off";
+
+            o.SetMemento(c.Memento);
+
+        }
+    }
+}
